554ad9646c0c359e2ec5b1b4b850a97c4eedfa17
anandjangalicsg <124046612+anandjangalicsg@users.noreply.github.com>
2023-03-15 16:11:06 +0530

    Converted spring jar to war app

--- src/main/java/com/example/azure/AzureApplication.java
+++ src/main/java/com/example/azure/AzureApplication.java
@@ -2,12 +2,19 @@ package com.example.azure;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @RestController
-public class AzureApplication {
+public class AzureApplication extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+            return application.sources(AzureApplication.class); // Replace DemoApplication with your main class
+    }
 
 	public static void main(String[] args) {
 		SpringApplication.run(AzureApplication.class, args);
